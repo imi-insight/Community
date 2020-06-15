5ae2f8e2fc99c5df0cd8eea018d34d2aa17037cc
Insight - The Data Science Society of IMI, New Delhi <58915167+imi-insight@users.noreply.github.com>
2020-06-15 18:52:11 +0530

    Data extraction example code in R

---
+++ Data-extraction-github.R
@@ -0,0 +1,5 @@
+# Data extraction example for github repo -
+
+link <- "https://raw.githubusercontent.com/imi-insight/Community/master/Multilinear-Regression/cars.csv"
+
+data <- read.csv(url(link))
